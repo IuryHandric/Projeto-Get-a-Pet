8d36f4ed0b57a37e14f395d7eb37e5087dce63ff
Iury Handric <handric11@hotmail.com>
2025-06-25 09:54:41 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const port = process.env.PORT || 3006;
+const app = express();
+
+// Config JSON response
+
+app.use(express.json());
+
+
+// Solve CORS
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+
+// Public folder for images
+app.use(express.static('public'))
+
+// Routes
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta http://localhost:${port}`)
+})

38278395ee91f12bda9c4be891148e96617185d3
Iury Handric <handric11@hotmail.com>
2025-06-30 19:59:12 -0300

    Criando busca de adoção e resgatando pet pelo ID

--- routes/Pet.Routes.js
+++ routes/Pet.Routes.js
@@ -11,5 +11,8 @@ const { imageUpload } = require('../helpers/image-upload')
 
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create)
 router.get('/', PetController.getAll)
+router.get('/mypets', verifyToken, PetController.getAllUserPets)
+router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
+router.get('/:id', PetController.getPetById)
 
 module.exports = router

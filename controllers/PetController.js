01bc86c7509f5b192dc3c684f178d92059bbb3db
Iury Handric <handric11@hotmail.com>
2025-07-01 10:13:21 -0300

    Removendo Pet do banco

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -127,14 +127,44 @@ module.exports = class PetController {
             return
         }
         // check if pet exists
-        const pet = await Pet.findOne({_id: id})
+        const pet = await Pet.findOne({ _id: id })
 
-        if(!pet) {
-            res.status(404).json({message: 'Pet não encontrado!'})
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' })
         }
         res.status(200).json({
             pet
         })
+    }
+
+    static async removePetById(req, res) {
+        const id = req.params.id
+
+        // check if id is valid
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID Inválido!' })
+            return
+        }
+
+        // check if pet exists
+        const pet = await Pet.findOne({ _id: id })
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' })
+        }
+
+        // check if logged in user registered the pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if(pet.user._id.toString() !== user._id.toString()) {
+            res.status(422).json({message: 'Houve um problema em processar a sua solicitação, tente novamente mais tarde!'})
+        }
+
+        await Pet.findByIdAndDelete(id)
+
+        res.status(200).json({message: 'Pet removido com sucesso!'})
+
     }
 
 }
